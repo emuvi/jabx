714ca26829e3afa428316589b43d694d826c5dbd
Éverton M. Vieira <emuvi@outlook.com.br>
2022-06-01 16:13:21 -0300

    adds alias on the joined tables

--- src/main/java/br/net/pin/jabx/data/Joined.java
+++ src/main/java/br/net/pin/jabx/data/Joined.java
@@ -7,6 +7,7 @@ import com.google.gson.Gson;
 public class Joined {
   public Joined.Ties ties;
   public Registry registry;
+  public String alias;
   public List<Filter> filters;
 
   public Joined() {
@@ -16,13 +17,24 @@ public class Joined {
     this.ties = ties;
   }
 
+  public Joined(Registry registry) {
+    this.registry = registry;
+  }
+
+  public Joined(Registry registry, String alias) {
+    this.registry = registry;
+    this.alias = alias;
+  }
+
   public Joined(Ties ties, Registry registry) {
     this.ties = ties;
     this.registry = registry;
   }
 
-  public Joined(Registry registry) {
+  public Joined(Ties ties, Registry registry, String alias) {
+    this.ties = ties;
     this.registry = registry;
+    this.alias = alias;
   }
 
   public Joined(Registry registry, List<Filter> filters) {
@@ -30,9 +42,22 @@ public class Joined {
     this.filters = filters;
   }
 
+  public Joined(Registry registry, String alias, List<Filter> filters) {
+    this.registry = registry;
+    this.alias = alias;
+    this.filters = filters;
+  }
+
   public Joined(Ties ties, Registry registry, List<Filter> filters) {
     this.ties = ties;
     this.registry = registry;
+    this.filters = filters;
+  }
+
+  public Joined(Ties ties, Registry registry, String alias, List<Filter> filters) {
+    this.ties = ties;
+    this.registry = registry;
+    this.alias = alias;
     this.filters = filters;
   }
 
